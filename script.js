cb3406dc97f747d4fbd090d73435082d416f9822
agguan <andrewg1153@gmail.com>
2024-08-23 11:00:09 -0400

    progress project

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
+const calculateSimpleInterest = (p, r, t) => {
+    return p * r * t/100;
+}
+
+const calculateTotalPayableAmount = (p, simpleInterest) => {
+    return p + simpleInterest;
+}
+
 const calculate = () => {
-  let p = document.getElementById("principle").value;
-  let r = document.getElementById("rate").value;
-  let t = document.getElementById("time").value;
-  let simpleInterest = (p * r * t) / 100;
-  let amount = p - simpleInterest;
+
+  let p = Number(document.getElementById("principal").value);
+  let r = Number(document.getElementById("rate").value);
+  let t = Number(document.getElementById("time").value);
+    //   let simpleInterest = (p * r * t) / 100;
+    //   let amount = p - simpleInterest;
+    // let simpleInterest = calculateSimpleInterest;
+    // let amount = calculateTotalPayableAmount;
+
+    let simpleInterest = calculateSimpleInterest(p, r, t);
+    let amount = calculateTotalPayableAmount(p, simpleInterest);
 
   let result = document.getElementById("result");
 
   result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
   <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
   <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
+
+  if (typeof module !== 'undefined')
+  module.exports = { calculateSimpleInterest, calculateTotalPayableAmount, calculate };
 };
+
